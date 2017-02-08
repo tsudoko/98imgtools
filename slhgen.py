e64af8c955d597f7dc285a83e0abfba91514ba49
flan <flan@flande.re>
2017-02-08 01:02:16 +0100

    Add default arguments for string variables in slhgen

--- slhgen.py
+++ slhgen.py
@@ -6,9 +6,9 @@ import sys
 from formats import slh
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--serial-number")
-parser.add_argument("--revision")
-parser.add_argument("--model-name")
+parser.add_argument("--serial-number", default='')
+parser.add_argument("--revision", default='')
+parser.add_argument("--model-name", default='')
 parser.add_argument("cylinders", type=int)
 parser.add_argument("heads", type=int)
 parser.add_argument("sectors", type=int)
@@ -16,15 +16,6 @@ parser.add_argument("infile")
 args = parser.parse_args()
 
 size = os.path.getsize(args.infile)
-
-if args.serial_number is None:
-    args.serial_number = ''
-
-if args.revision is None:
-    args.revision = ''
-
-if args.model_name is None:
-    args.model_name = ''
 
 sectorsize = 512
 header = slh.header.pack(
